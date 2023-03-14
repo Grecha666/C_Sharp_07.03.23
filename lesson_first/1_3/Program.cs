1b50a9d3697dda852a216fa81519eb8ecac80a54
Anya Grechina <drivepie91@mail.ru>
2023-03-14 19:39:20 +0300

    Task 1_3

---
+++ lesson_first/1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает ВСЕ целые числа от -N до N
+
+Console.WriteLine("Напишите целочисленное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = -number;
+
+while (count != number)
+{
+    Console.WriteLine(count);
+    if (number < 0)
+    {
+    count--;
+    }
+    else
+    {
+    count++;
+    }
+}

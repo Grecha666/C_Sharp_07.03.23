c2e618f964460742445596566e32070c2a437b75
Anya Grechina <drivepie91@mail.ru>
2023-03-14 17:25:49 +0300

    Task 1_0

---
+++ lesson_first/1_0/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт ег квадрат (число умноженное само на себя)
+
+Console.WriteLine("Напишите число: "); //"Write a number: "
+int number = int.Parse(Console.ReadLine()!); //int.Parse - преобразователь из строки в целочисленное значение (из string  в int)
+Console.WriteLine(number*number);

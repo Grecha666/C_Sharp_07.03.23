97e4ab6401365a1b708f32c3d52d3f242a7a2f1b
Anya Grechina <drivepie91@mail.ru>
2023-03-18 20:04:18 +0300

    Task 2_3

---
+++ lesson_second/2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+
+int a = new Random().Next(100,1000);
+
+void Function(int num)
+
+{   Console.WriteLine(num); 
+ 
+    if(num%7==0 && num%23==0)
+  
+        {
+            Console.WriteLine("Твоё число подходит");
+        }
+    else
+        {
+            Console.WriteLine("Твоё число не кратно 7 и 23");
+        }
+}
+
+Function(a);

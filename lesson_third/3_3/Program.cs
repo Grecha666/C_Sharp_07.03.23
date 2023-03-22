eea3db2bcdbb051f08312032493d91760dc2e303
Anya Grechina <drivepie91@mail.ru>
2023-03-22 13:28:55 +0300

    Task 3_3

---
+++ lesson_third/3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+void SumOfSquares(int number)
+{
+    int index = 1;
+    while(index <= number)
+    {
+        Console.WriteLine(Math.Pow(index,2));
+        index++;
+    }
+}    
+
+Console.WriteLine("Напишите своё число: ");
+int num = int.Parse(Console.ReadLine()!);
+SumOfSquares(num);

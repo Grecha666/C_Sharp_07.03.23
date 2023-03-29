1f165f32b85c4f8c2c262ab0db90c4b436b21932
Anya Grechina <drivepie91@mail.ru>
2023-03-29 20:06:34 +0300

    Task 3_1

---
+++ lesson_third/3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y])
+
+void Quarters(int num)
+{
+    if (num == 1)
+    {    
+        Console.WriteLine("Значения x > 0, y > 0");
+    }    
+    else if(num == 2)
+    {
+        Console.WriteLine("Значения x < 0, y > 0");
+    }
+    else if(num == 3)
+    {
+       Console.WriteLine("Значения x < 0, y < 0"); 
+    }
+    else if(num == 4)
+    {
+       Console.WriteLine("Значения x > 0, y < 0");
+    }
+    else 
+    {
+       Console.WriteLine("ЭРОР!!! Ты написал число, не соответствующее номеру четверти!");
+    }
+}
+
+Console.WriteLine("Напиши число, соответствующее номеру четверти (1, 2, 3 или 4) "); 
+int quarter_number = int.Parse(Console.ReadLine()!);
+
+Quarters(quarter_number);
+

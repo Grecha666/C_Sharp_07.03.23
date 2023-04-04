28b64bfc6dcad467113b3b989de5299a9cc02e5a
Anya Grechina <drivepie91@mail.ru>
2023-04-04 14:59:50 +0300

    Task 5_1

---
+++ lesson_ fifth/5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот
+
+Console.WriteLine("Напишите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Начало диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Конец диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int[] MakeArray(int c, int m, int n)
+{
+    int[] array = new int [c];
+
+    for (int i=0; i < array.Length; i++)
+    array[i] = new Random().Next(m, n+1);
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i=0; i < arr.Length; i++)
+    Console.Write($"{arr[i]} ");
+}
+
+
+void MyFunction(int[] arr) 
+{
+    for (int i=0; i < arr.Length; i++)
+    arr[i]*=-1;
+
+}
+
+int[] array = MakeArray(count, a, b);
+PrintArray(array);
+Console.WriteLine();
+MyFunction(array);
+PrintArray(array);

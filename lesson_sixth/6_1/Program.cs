9713bb9243f8f5e36dea7755b28f3068cf0a9a4b
Anya Grechina <drivepie91@mail.ru>
2023-04-11 21:23:15 +0300

    Task 6_1

---
+++ lesson_sixth/6_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины
+// Теорема о неравенстве треугольника: каждая сторона меньше суммы двух других сторон
+
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("c = ");
+int c = int.Parse(Console.ReadLine()!);
+
+bool MyFunction(int a, int b, int c)
+{
+    if(a < b + c && b < a + c && c < a + b)
+    return true;
+    else
+    return false;
+}
+
+Console.WriteLine(MyFunction(a, b, c));

5d4394abdcc5c5da366c5495b53f9d3a18ff3270
Anya Grechina <drivepie91@mail.ru>
2023-04-23 18:57:31 +0300

    Task 7_0.1

---
+++ lesson_seventh/Home_Work/7_0.1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Метод заполняющий двумерный массив и метод его выводящий
+
+void PrintArray(int[,]arr) //заполняющий метод
+{
+    int rows_size = arr.GetLength(0);
+    int colums_size = arr.GetLength(1);
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int rows, int colums, int from, int to) //выводящий метод
+{
+    int[,] arr = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            arr[i,j] = new Random().Next(from, to + 1);
+        }
+    }
+    return arr;
+}
+    
+Console.WriteLine("Напиши количество строк: ");
+int rows_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Напиши количество столбцов: ");
+int colums_num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Напиши начало: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Напиши конец: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] array = MassNums(rows_num, colums_num, start, stop);
+PrintArray(array);

962e8daabedcf9c525a265abef458d98d9de6bdc
Anya Grechina <drivepie91@mail.ru>
2023-04-23 19:22:25 +0300

    Task 7_1

---
+++ lesson_seventh/7_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размера m*n, каждый элемент в массиве находится по формуле А = m + n. Выведите полученный результат на экран
+
+void PrintArray(int[,]arr) //заполняющий метод
+{
+    int rows_size = arr.GetLength(0);
+    int colums_size = arr.GetLength(1);
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int rows, int colums) //выводящий метод
+{
+    int[,] arr = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            arr[i,j] = i + j;
+        }
+    }
+    return arr;
+}
+    
+Console.WriteLine("Напиши количество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Напиши количество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = MassNums(m, n);
+PrintArray(array);

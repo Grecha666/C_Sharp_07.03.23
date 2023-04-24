62f0eb844ea7bba274b88ee42fd37a8fbd70b12c
Anya Grechina <drivepie91@mail.ru>
2023-04-24 19:54:20 +0300

    Task 9_0

---
+++ lesson_ninth/9_0/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии
+
+void PrintNuns(int n)
+{
+    if (n == 0) return;
+    PrintNuns(n - 1);
+    Console.Write($"{n} "); 
+}
+
+Console.Write("Напиши число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Result: ");
+PrintNuns(N);
+
+
